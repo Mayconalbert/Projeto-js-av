82d8a74994d64b041c6c7eb2d8d29401dfd8d9aa
Maicon Silva Albert <mayconalbert188@gmail.com>
2026-03-26 23:20:57 -0300

    [27/03/26]bog-resolvido-porta5500eraporta8081

--- ajax/server.js
+++ ajax/server.js
@@ -4,7 +4,29 @@ const app = express()
 
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json()),
+app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('ok'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) =>{
+    upload(req, res, err => {
+        if (err){
+            return res.end('Ocorreu um erro.')
+        }
+        res.end('concluido com sucesso')
+    })
+})
+
+
 app.listen(8081, () => console.log('Executando...'))
