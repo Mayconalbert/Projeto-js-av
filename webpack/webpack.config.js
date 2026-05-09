f0f817c86f71abdaa8cef2bb5219f490f9dbc324
Maicon <mayconalbert188@gmail.com>
2026-05-09 15:33:27 -0300

    09/05/26- mebpack-fim

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,31 @@
+const modoDev = process.env.NODE_ENV !== 'production'  
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: path.resolve(__dirname + '/public')
     }, 
+
+     devServer: {
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        port: 9000
+    },
+   
+    optimization: {
+        minimize: !modoDev,
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    },
         plugins: [
             new MiniCssExtractPlugin({
                 filename: 'estilo.css'
@@ -23,11 +41,12 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
+                test: /\.s?[ac]ss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     //'style-loader', 
-                    'css-loader'
+                    'css-loader',
+                    'sass-loader'
                 ]
             }
         ]
