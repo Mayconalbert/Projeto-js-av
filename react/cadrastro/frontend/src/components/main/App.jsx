8aa9b54391b42ad8b5f1c7bd638349d776547fc6
Maicon <mayconalbert188@gmail.com>
2026-06-15 22:40:27 -0300

    14/06/26-ColocadoAs0pastApp e feiro a integração entre elas

--- react/cadrastro/frontend/src/components/main/App.jsx
+++ react/cadrastro/frontend/src/components/main/App.jsx
@@ -3,10 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 
-import Logo from './Logo'
-import Nav from './Nav'
+import Logo from '../template/Logo'
+import Nav from '../template/Nav'
 import Home from '../home/Home'
-import Footer from './Footer'
+import Footer from '../template/Footer'
 
 export default (props) => (
     <div className="app">
