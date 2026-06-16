79f44befa9669b50e1cd4ae381296547592faebf
Maicon <mayconalbert188@gmail.com>
2026-06-15 23:04:37 -0300

    15/06/26-Rota

--- react/cadrastro/frontend/src/components/main/App.jsx
+++ react/cadrastro/frontend/src/components/main/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 
+import { HashRouter } from 'react-router-dom'
 import Logo from '../template/Logo'
 import Nav from '../template/Nav'
 import Home from '../home/Home'
