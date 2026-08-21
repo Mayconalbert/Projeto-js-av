d502426bce339b16274012d6b3f0079f039a0942
Maicon <mayconalbert188@gmail.com>
2026-08-21 00:22:57 -0300

    21/08/2026 Configurando os componentes dos botão

--- vue/calculator/src/main/CalculatorMain.vue
+++ vue/calculator/src/main/CalculatorMain.vue
@@ -1,9 +1,24 @@
 <template>
     <div class="calculator">
         <CalculatorDisplay />
-        <CalculatorButton />
-        <CalculatorButton />
-       
+        <CalculatorButton  label="AC" triple />
+        <CalculatorButton  label="/" operation />
+        <CalculatorButton  label="7" />
+        <CalculatorButton  label="8" />
+        <CalculatorButton  label="9" />
+        <CalculatorButton  label="*" operation />
+        <CalculatorButton  label="4" />
+        <CalculatorButton  label="5" />
+        <CalculatorButton  label="6" />
+        <CalculatorButton  label="-" operation />
+        <CalculatorButton  label="1" />
+        <CalculatorButton  label="2" />
+        <CalculatorButton  label="3" />
+        <CalculatorButton  label="+" operation />
+        <CalculatorButton  label="0" double />
+        <CalculatorButton  label="."  />
+        <CalculatorButton  label="=" operation />
+        
     </div>
   
 </template>
