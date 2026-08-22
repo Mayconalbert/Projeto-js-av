9b27338edcb9e11d678aad2a261ea68e56e2c6d3
Maicon <mayconalbert188@gmail.com>
2026-08-22 00:02:20 -0300

    22/08/2026 Implementação da logica e o termino da calculadora Vue

--- vue/calculator/src/main/CalculatorMain.vue
+++ vue/calculator/src/main/CalculatorMain.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="calculator">
-    <CalculatorDisplay value="1000" />
+    <CalculatorDisplay :value="displayValue" />
     <CalculatorButton label="AC" triple @onClick="clearMemory" />
     <CalculatorButton label="/" operation @onClick="setOperation" />
     <CalculatorButton label="7" @onClick="addDigit" />
@@ -10,11 +10,11 @@
     <CalculatorButton label="4" @onClick="addDigit" />
     <CalculatorButton label="5" @onClick="addDigit" />
     <CalculatorButton label="6" @onClick="addDigit" />
-    <CalculatorButton label="-" operation />
+    <CalculatorButton label="-" operation @onClick="setOperation" />
     <CalculatorButton label="1" @onClick="addDigit" />
     <CalculatorButton label="2" @onClick="addDigit" />
     <CalculatorButton label="3" @onClick="addDigit" />
-    <CalculatorButton label="+" operation />
+    <CalculatorButton label="+" operation @onClick="setOperation" />
     <CalculatorButton label="0" double @onClick="addDigit"  />
     <CalculatorButton label="." @onClick="addDigit" />
     <CalculatorButton label="=" operation @onClick="setOperation" />
@@ -25,16 +25,80 @@
 import CalculatorDisplay from "../components/CalculatorDisplay.vue";
 import CalculatorButton from "../components/CalculatorButton.vue";
 export default {
+    data: function() {
+        return{
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+
+        }
+    },
   components: { CalculatorButton, CalculatorDisplay },
   methods: {
     clearMemory() {
-      console.log('Limpar memória');
+        Object.assign(this.$data, this.$options.data())
+     
     },
     setOperation(operation) {
-      console.log('Operation ' + operation);
+        if(this.current === 0) {
+            this.operation = operation
+            this.current = 1
+            this.clearDisplay = true
+        } else {
+            const equals = operation === "="
+            const currentOperation = this.operation
+
+            try{
+                this.values[0] = eval(
+                    `${this.values[0]} ${currentOperation} ${this.values[1]}`);
+                    if (isNaN(this.values[0]) || !isFinite(this.values[0])) {
+                        this.clearMemory();
+                        return;
+                    }
+            }
+            catch (e) {
+                this.$emit('onError', e)
+            }
+
+            this.values[1] = 0
+
+            this.displayValue = this.values[0]
+            this.operation = equals ? null : operation   
+            this.current = equals ? 0 : 1
+            this.clearDisplay = !equals
+        }
+      
     },
     addDigit(n) {
-      console.log('Digito ' + n);
+        if(n === "." && this.displayValue.includes(".")) {
+            return 
+        }
+
+        const clearDisplay = this.displayValue === "0"
+                || this.clearDisplay
+        const currentValue = clearDisplay ? "" : this.displayValue
+        const displayValue = currentValue + n
+
+        // Altenartiva 1
+
+        this.displayValue = displayValue
+        this.clearDisplay = false
+        this.values[this.current] = displayValue
+
+        //Altenartiva 2 com numero Float
+
+        // if(n !== ".") {
+        //     const i = this.current
+        //     const newValue = parseFloat(displayValue)
+        //     this.values[i] = newValue
+        // }
+      
+        
+
+
+      
     },
   }
 };
