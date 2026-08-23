df3de85a0b1d2053090bc8546cc06d9b5a85a93f
Maicon <mayconalbert188@gmail.com>
2026-08-22 23:30:06 -0300

    22/08/2026 Feito um componente presente

--- vue/montyhall/vue.config.js
+++ vue/montyhall/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false // Isso desativa os erros do ESLint ao salvar o arquivo
 })
