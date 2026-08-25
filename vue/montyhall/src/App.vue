196baf76549d6096a1e7f5fbab15788fdfda4cc9
Maicon <mayconalbert188@gmail.com>
2026-08-24 23:21:42 -0300

    24/08/2026 Inicializando tela principal do jogo Monty Hall

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-    <Gift />
+    <Door number="1" hasGift="false" />
   </div>
 
 </template>
 
 <script>
-import Gift from './components/GiftBox'
+import Door from './components/Door.vue';
+
 export default {
     name: 'App',
-    components: { Gift }
+    components: { Door }
 
 }
 </script>
