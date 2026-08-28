0518a1da4ba83307dd83e2186c762c507f4ae537
Maicon <mayconalbert188@gmail.com>
2026-08-27 22:34:43 -0300

    27/08/2026 Inserindo dados no meu banco de dados

--- banco-relacional/crieTabelaEstado.sql
+++ banco-relacional/crieTabelaEstado.sql
@@ -1,5 +1,12 @@
---Criando tabela estado!
-create table estado {
+DROP TABLE IF EXISTS estado;
+-- Criando tabela estado!
+create table estados (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
-}
+    sigla varchar(45) NOT NULL,
+    regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
+    populacao INT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (nome),
+    UNIQUE KEY (sigla)
+);
---
+++ banco-relacional/inserirEstados1.sql
@@ -0,0 +1,8 @@
+insert into estado (nome, sigla, região, populacao)
+VALUE ('Acre', 'AC', 'NORTE', 683000);
+
+insert into estado (nome, sigla, região, populacao)
+VALUE 
+    ('Alagoas', 'AL', 'Nordeste', 33833),
+    ('Amapá', 'AP', 'Norte', 80000),
+    ('Amozonas', 'AM', 'Norte', 40006);
