e1a74128cee23e2a3865e077cec75ff5a9fe0184
Maicon <mayconalbert188@gmail.com>
2026-08-27 23:53:25 -0300

    2/08/2026 Enviado para o banco de dados todos os estados

---
+++ banco-relacional/inserirEstados2.sql
@@ -0,0 +1,26 @@
+-- mais siglas
+
+insert into estado 
+    (nome, sigla, região, populacao)
+
+VALUES
+    ('Bahia', 'BA', 'Nordeste', 40000),
+    ('Ceara', 'CE', 'Nordeste', 2000),
+    ('Distrito Federal', 'DF', 'Centro-Oeste', 20000),
+    ('Espirito Santos', 'ES', 'Sudeste', 8000 ),
+    ('Goias', 'GO', 'Centro-Oeste', 4000),
+    ('Maranhão', 'MA', 'Nordeste', 334000),
+    ('Mato Grosso', 'MT', 'Centro-Oeste', 5000),
+    ('Mato Grosso do Sul', 'MS', 'Centro-Oeste', 20000),
+    ('Minas Gerais', 'MG', 'Sudeste', 200000),
+    ('Para', 'PA', 'Norte', 9000),
+    ('Paraiba', 'PR', 'Nordeste', 8000),
+    ('Parana', 'PaR', 'Sul', 33000),
+    ('Pernambuco', 'PE', 'Nordeste', 399000),
+    ('Piaui', 'PI', 'Nordeste', 20000),
+    ('Rio de Janeiro', 'Rj', 'Sudeste', 29900),
+    ('Rio grande do Norte', 'RM', 'Nordeste', 80000 ),
+    ('Rio grande do Sul', 'RS', 'Sul', 20000),
+    ('Rondônia', 'RO', 'Norte', 20000000 ),
+    ('Santa Catarina', 'SC', 'Sul', 122300);
+
