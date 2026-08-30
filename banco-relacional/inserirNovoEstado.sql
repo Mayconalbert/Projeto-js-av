672b7ffd7f013344f013f0897b52569c2b8f4bfd
Maicon <mayconalbert188@gmail.com>
2026-08-30 00:46:02 -0300

    30/08/2026 Inserir novos dados

---
+++ banco-relacional/inserirNovoEstado.sql
@@ -0,0 +1,9 @@
+insert into `estado` (nome, sigla, região, populacao)
+values ('Novo3', 'NV3', 'Sul', 20000 );
+
+
+insert into `estado` (nome, sigla, região, populacao)
+values ('Mais novo2', 'MN2', 'Norte', 44000);
+
+
+select * from `estado`;
