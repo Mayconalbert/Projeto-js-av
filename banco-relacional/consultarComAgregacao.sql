4ec7dfbb0c87b5db9458f468011117d7c564f162
Maicon <mayconalbert188@gmail.com>
2026-08-31 22:24:38 -0300

    31/08/2026 Consulta da população por soma e média

---
+++ banco-relacional/consultarComAgregacao.sql
@@ -0,0 +1,11 @@
+select 
+    região as 'Região', // pega os dados da coluna região e renomeia para Região
+    sum(populacao) as 'Total' // sum e uma função de agregação que soma os valores da coluna
+from estado // pega os dados da tabela estado
+group by região // agrupa ps dados da coluna região e vai somar os valor de cada região
+order by Total desc; // ordena os valores da coluna Total de forma decrescente
+
+
+select 
+    avg(populacao) as 'Populacao Média' // avg e uma função de agregação que calcula a média 
+from estado 
