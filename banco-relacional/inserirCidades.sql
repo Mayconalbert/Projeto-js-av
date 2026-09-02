bc9f35da9223a4aea8916a1108c622162791b2d1
Maicon <mayconalbert188@gmail.com>
2026-09-01 21:32:28 -0300

    01/09/2026 Inserindo cidades nas capitais dos instados

---
+++ banco-relacional/inserirCidades.sql
@@ -0,0 +1,15 @@
+INSERT INTO cidades (nome, area, estado_id) // insere dados na tabela cidades
+values ('Curitiba', 100, 31); // insere o nome da cidade que sera inserido no estado da capital 
+
+insert into cidades (nome, area, estado_id)
+values ('Niteroi', 150, 25); 
+
+insert into cidades (nome, area, estado_id)
+values ('Caruaru', 200, (select id from estado where sigla = 'PE')); // seleciona o id da capital com id sigla 
+
+insert into cidades (nome, area, estado_id)
+values ('Juaceiro do norte', 300, (select id from estado where sigla = 'CE'));
+
+
+select * from cidades; // mostra todos os dados da tabela da cidade
+
