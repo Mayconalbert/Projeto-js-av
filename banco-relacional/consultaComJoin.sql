a7aae0c2633a94011dedbc557c1e873a91a02f99
Maicon <mayconalbert188@gmail.com>
2026-09-01 23:53:08 -0300

    01/09/2026 tirando as // para colocar -- nos comentários

--- banco-relacional/consultaComJoin.sql
+++ banco-relacional/consultaComJoin.sql
@@ -1,13 +1,13 @@
 select 
-    e.nome as Estado, // cria uma coluna com nome Estado
-    c.nome as Cidade, // cria nome de coluna Cidade
-    região as Região // cria coluna Região
- from estado e, cidades c // pega os dados da tabela estado e cidades
-where e.id = c.estado_id; // concatena os dados da tabela estado e cidade, onde o id do estado é igual ao id do estado na tabela cidades
+    e.nome as Estado, -- cria uma coluna com nome Estado
+    c.nome as Cidade, -- cria nome de coluna Cidade
+    região as Região -- cria coluna Região
+ from estado e, cidades c -- pega os dados da tabela estado e cidades
+where e.id = c.estado_id; -- concatena os dados da tabela estado e cidade, onde o id do estado é igual ao id do estado na tabela cidades
 
 select 
     e.nome as estado,
     c.nome as Cidade,
     região as  região
 from estado e
-inner join cidades c on e.id = c.estado_id // Forma de fazer o join entre as tabelas estado e cidades, onde o id do estado é igual ao id do estado na tabela cidade
+inner join cidades c on e.id = c.estado_id -- Forma de fazer o join entre as tabelas estado e cidades, onde o id do estado é igual ao id do estado na tabela cidade
