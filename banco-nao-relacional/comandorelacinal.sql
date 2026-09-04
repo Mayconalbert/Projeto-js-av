5cbc53c944b93ef8b29642129811b67ca7be4c7c
Maicon <mayconalbert188@gmail.com>
2026-09-04 00:04:04 -0300

    04/09/2026 Criando banco de dados não relacional

---
+++ banco-nao-relacional/comandorelacinal.sql
@@ -0,0 +1,5 @@
+mongo --entrar na linha de comando shell
+show dbs -- mostra o banco de dados
+use wm -- criar ou mudar de banco de dados
+db.createCollection('estado') --criar tabela
+db.estado.drop() -- excluir tabela
